729a71df192368372da7a1af41dc13c214209b65
adrianilie19 <ilieadrian340@yahoo.com>
2026-02-27 11:44:35 +0100

    Realizado la exportación a CSV

--- src/ui/exporter.py
+++ src/ui/exporter.py
@@ -15,7 +15,17 @@ def exportar_json(datos, nombre_archivo):
 def exportar_csv(datos, nombre_archivo):
     if not datos:
         print("No hay datos para crear el CSV")
-
+        return
+    try:
+        with open(nombre_archivo + ".csv", "w", newline='', encoding="utf-8") as f:
+            campos = list(datos[0].keys())
+            escribirCSV = csv.DictWriter(f, fieldnames=campos)
+            escribirCSV.writeheader()
+            for fila in datos:
+                escribirCSV.writerow(fila)
+        print(f"Se ha guardado el archivo en formato CSV: {nombre_archivo}.csv")
+    except:
+        print("Error al guardar CSV")
 
 #exportar y generar reporte en pdf
 def exportar_pdf(datos, nombre_archivo):
