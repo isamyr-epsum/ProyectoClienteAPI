58268e93f7daf4538abaced307d94e305b1db387
luckynoesta <luc14bhz@hotmail.com>
2026-03-02 11:15:41 +0100

    Data Clima con interfaz de TKINTER

--- main.py
+++ main.py
@@ -1,45 +1,32 @@
 """
-Archivo principal de la aplicación DataClima
+Archivo principal de la aplicación DataClima Con Tkinter
 Autor: Equipo (Isamir, Lucas, Josue, Adrian)
 Sprint 2 - Febrero 2025
-
-Punto de entrada de la aplicación
 """
 
 import tkinter as tk
-
-from src.data import storage_main
-from src.api import weather_main
-from src.ui import exporter_main
+from src.ui.menu import AplicacionClima
 
 
 def main():
-    while True:
-        print("--- MENU PRINCIPAL ---")
-        print("1 - Consultar")
-        print("2 - Historial")
-        print("3 - Exportar")
-        print("4 - Salir")
-        opcion = input("Escoga una opción -- > ")
-        if opcion == "1":
-            weather_main()
-        if opcion == "2":
-            storage_main()
-        if opcion == "3":
-            exporter_main()
-        if opcion == "4":
-            print("Adios")
-            break
+    """Inicia la aplicación con interfaz gráfica"""
 
-main()
+    print("=" * 50)
+    print(" DATACLIMA - Aplicación Meteorológica")
+    print("=" * 50)
+    print("\nIniciando interfaz gráfica...\n")
 
-# if __name__ == "__main__":
-#     # Crear la ventana principal
-#     # root = tk.Tk ()
-#     mostrar_menu_principal()
-#     # Iniciar la aplicación
-#     # app = mostrar_menu_principal()
-#     # app.mainloop()
-#
-#     # Ejecutar el loop de la interfaz
-#     # root.mainloop()
+    # Creamos ventana
+    root = tk.Tk()
+
+    # Iniciar aplicación
+    app = AplicacionClima(root)
+
+    # Ejecutar
+    root.mainloop()
+
+    print("\nAplicación cerrada. Hasta pronto!!")
+
+
+if __name__ == "__main__":
+    main()
